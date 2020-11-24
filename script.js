38b37734a5a7aa00528a8b211c889d3487cc7044
Xuejun-Pei <74582928+Xuejun-Pei@users.noreply.github.com>
2020-11-24 18:12:49 +0800

    3

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ alert("Hello!")
 $("p").mouseout(function(){
 	$(this).html("where did you go?")
 	})
+
+$(".menu").click(function(){
+
+
+
+
+
+	if ($(this).hasClass("open")){
+		$(this).removeClass("open")
+	}else{
+		$(this).addClass("open")
+	}
+})
